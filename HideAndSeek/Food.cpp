4b0f7323ea841dd9d19330a283bb8bfde54bb1e3
rogrammer <96201583+rogrammer@users.noreply.github.com>
2023-05-28 13:40:48 +0300

    Update Food.cpp
    random move of food

--- HideAndSeek/Food.cpp
+++ HideAndSeek/Food.cpp
@@ -1,7 +1,22 @@
 #include "Food.h"
+#include <stdlib.h>
 
 Food::Food(QGraphicsItem *parent)
 {
     setRect(-25, -25, 50, 50);
-    
+    connect(&timer, &QTimer::timeout, this, &Food::moveRandom);
+    timer.start(500);
+}
+
+Food::~Food()
+{
+    timer.stop();
+}
+
+void Food::moveRandom()
+{
+    if(x() < 50 || x() > 740 || y() < 50 || y() > 540)
+        setPos(rand() % 500 + 50, rand() % 400 + 50);
+
+    setPos(x() + (rand() % 100 - 50), y() + (rand() % 100 - 50));
 }
