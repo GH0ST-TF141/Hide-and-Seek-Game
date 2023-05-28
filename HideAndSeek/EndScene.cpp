3a217494cc2082a038334c084709da075e98d886
rogrammer <96201583+rogrammer@users.noreply.github.com>
2023-05-28 13:55:12 +0300

    Update EndScene.cpp
    fit the picture to the screen
    available the exception messages

--- HideAndSeek/EndScene.cpp
+++ HideAndSeek/EndScene.cpp
@@ -10,27 +10,31 @@ EndScene::EndScene(QString text)
 {
     setSceneRect(0, 0, 800, 600);
     ButtonItem *restart = new ButtonItem("RESTART");
-    restart->setPos(180, 400);
+    restart->setPos(100, 400);
     ButtonItem *quit = new ButtonItem("QUIT");
-    quit->setPos(180, 300);
-    Score *gameOver = new Score("GAME OVER", 100, 0, 30);
-    Score *win = new Score(text, 120, 70);
+    quit->setPos(100, 300);
+    Score *gameOver = new Score("GAME OVER", 100, 50, 30);
+    Score *win = new Score(text, 120, 100);
+
     try {
-    QPixmap image(":images/gameover");
-        if(image.isNull()) {
+        QPixmap image(":images/gameover");
+        if (image.isNull()) {
             throw std::runtime_error("exception occur!");
         }
-        QGraphicsPixmapItem* pixmapItem = new QGraphicsPixmapItem(image);
+        QPixmap scaledImage = image.scaled(790, 590);
+        QGraphicsPixmapItem* pixmapItem = new QGraphicsPixmapItem(scaledImage);
         addItem(pixmapItem);
-        //qDebug << "loaded succesfully completed!";
-    } catch(std::exception e) {
-        //qWarning << e;
+        qDebug() << "loaded successfully completed!";
+    } catch(std::exception& e) {
+        qWarning() << e.what();
     }
 
     addItem(gameOver);
     addItem(restart);
     addItem(quit);
     addItem(win);
+
+    // Connect the buttons to their respective slots
 }
 
 void EndScene::restart()
