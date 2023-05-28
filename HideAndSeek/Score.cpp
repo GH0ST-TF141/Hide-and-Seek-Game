2ed499bfcbe5e52765c9455ff58e4bff08c33394
rogrammer <96201583+rogrammer@users.noreply.github.com>
2023-05-28 20:04:33 +0300

    Update Score.cpp
    change default color

--- HideAndSeek/Score.cpp
+++ HideAndSeek/Score.cpp
@@ -8,6 +8,7 @@ Score::Score(QString text, int x, int y, int font)
     appearance.setFamily("Arial");
     appearance.setPointSize(font);
     appearance.setBold(true);
+    setDefaultTextColor(Qt::white);
     setFont(appearance);
     setPlainText(text);
 }
