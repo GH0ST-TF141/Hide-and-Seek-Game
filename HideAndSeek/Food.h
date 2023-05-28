1d5f96155469328be25dcfe13d8c2f9714b4fe9d
rogrammer <96201583+rogrammer@users.noreply.github.com>
2023-05-28 13:40:04 +0300

    Update Food.h
    random move of food

--- HideAndSeek/Food.h
+++ HideAndSeek/Food.h
@@ -1,11 +1,23 @@
 #ifndef FOOD_H
 #define FOOD_H
-#include <QGraphicsEllipseItem>
 
-class Food : public QGraphicsEllipseItem {
+#include <QGraphicsEllipseItem>
+#include <QObject>
+#include <QTimer>
+
+class Food : public QObject, public QGraphicsEllipseItem
+{
+    Q_OBJECT
+
 public:
     Food(QGraphicsItem *parent = nullptr);
+    ~Food();
 
+public slots:
+    void moveRandom();
+
+private:
+    QTimer timer;
 };
 
 #endif // FOOD_H
