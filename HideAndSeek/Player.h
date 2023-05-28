c53b7bba0b27ca3b8ae897dd216d40a09a5fd6c1
rogrammer <96201583+rogrammer@users.noreply.github.com>
2023-05-28 20:02:49 +0300

    Update Player.h

--- HideAndSeek/Player.h
+++ HideAndSeek/Player.h
@@ -4,15 +4,16 @@
 #include <QGraphicsPixmapItem>
 #include <QKeyEvent>
 #include <vector>
+#include <QPainter>
 
 class Player : public QGraphicsPixmapItem {
 public:
     Player();
-
+    QRectF boundingRect() const;
 protected:
     void keyPressEvent(QKeyEvent *event) override;
     void keyReleaseEvent(QKeyEvent *event) override;
-
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 private:
     std::vector<int> activeKeys;
 };
