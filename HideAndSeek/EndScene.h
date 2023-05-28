dd1b5f1ca07cf74a60bdd7b6482829483d8c0fe3
rogrammer <96201583+rogrammer@users.noreply.github.com>
2023-05-28 19:58:59 +0300

    Update EndScene.h
    avaible for signals

--- HideAndSeek/EndScene.h
+++ HideAndSeek/EndScene.h
@@ -1,10 +1,13 @@
 #ifndef ENDSCENE_H
 #define ENDSCENE_H
+
 #include <QGraphicsScene>
 
 class EndScene : public QGraphicsScene {
+    Q_OBJECT
 public:
     EndScene(QString text);
+private slots:
     void restart();
     void quit();
 };
