ac43c3d0813fa136a40b7f594432f097a9d71c84
Furkan-Demir <87570239+GH0ST-TF141@users.noreply.github.com>
2023-05-30 17:16:08 +0300

    Update Player2.h

--- HideAndSeek/Player2.h
+++ HideAndSeek/Player2.h
@@ -1,12 +1,21 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-#include <QGraphicsEllipseItem>
-#include <QKeyEvent>
+#ifndef PLAYER2_H
+#define PLAYER2_H
 
-class Player2 : public QGraphicsEllipseItem {
+#include <QGraphicsPixmapItem>
+#include <QKeyEvent>
+#include <vector>
+#include <QPainter>
+
+class Player2 : public QGraphicsPixmapItem {
 public:
     Player2();
+    QRectF boundingRect() const;
+protected:
     void keyPressEvent(QKeyEvent *event) override;
+    void keyReleaseEvent(QKeyEvent *event) override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+private:
+    std::vector<int> activeKeys;
 };
 
-#endif // PLAYER_H
+#endif // PLAYER2_H
